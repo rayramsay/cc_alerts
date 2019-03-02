f62c0bb0a6dd66d84dc762823d626573a876c39a
Rachel Ramsay <rachel.ramsay@outlook.com>
2019-03-01 19:09:06 -0800

    Fix clean_summary & convert struct_time to local tz datetime

--- feed.py
+++ feed.py
@@ -1,47 +1,67 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pprint
 import re
 import ssl
+from datetime import datetime
+from time import mktime
 
 import feedparser
+from dateutil import tz
 
-cleanr = re.compile('&nbsp;|<.*?>')
-def remove_html(raw_html):
-    return re.sub(cleanr, '', raw_html)
-def remove_empty_lines(raw_text):
-    return "\n".join([ll.rstrip() for ll in raw_text.splitlines() if ll.strip()])
-def clean_summary(summary):
-    return remove_empty_lines(remove_html(summary))
+### TEXT CLEANING HELPERS ######################################################
+html_tag = re.compile('<.*?>')
+html_entitites = {
+    '&nbsp;' : ' ', '&amp;' : '&', '&quot;' : '"',
+    '&lt;'   : '<', '&gt;'  : '>'
+}
 
-
-pp = pprint.PrettyPrinter()
-# pttrn = re.compile("Train.*(523|542)", flags=re.IGNORECASE)
-pttrn = re.compile("systemwide", flags=re.IGNORECASE)
+def clean_summary(raw_text):
+    # Remove HTML tags.
+    raw_text = re.sub(html_tag, '', raw_text)
+    # Remove HTML entities.
+    for (k, v) in html_entitites.items():
+        raw_text = raw_text.replace(k, v)
+    # Remove whitespace characters (space, tab, newline, return, formfeed).
+    return " ".join(raw_text.split())
+### TIME CONSTANTS #############################################################
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
+################################################################################
 
 # https://stackoverflow.com/questions/28282797/feedparser-parse-ssl-certificate-verify-failed
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
 
 # https://www.pythonforbeginners.com/feedparser/using-feedparser-in-python
-url = 'https://public.govdelivery.com/topics/CACCTRAN_2/feed.rss'
-feed = feedparser.parse(url)
-for entry in feed.get("entries"):
-    if pttrn.search(entry.get("summary")):
-        print(entry.get("published_parsed"))  #time.struct_time
-        summary = clean_summary(entry.get("summary"))
-        print(summary)
-
-
-       # TODO sometimes strings have html in them, sometimes they have \xa0
-       # https://stackoverflow.com/questions/26068832/how-to-remove-this-xa0-from-a-string-in-python
-
 # https://stackoverflow.com/questions/22211795/python-feedparser-how-can-i-check-for-new-rss-data
+
+pttrn = re.compile("systemwide|Train.*(523|542)", flags=re.IGNORECASE)
+url = 'https://public.govdelivery.com/topics/CACCTRAN_2/feed.rss'
+
+last_etag = None
+last_modified = None
+feed = feedparser.parse(url, etag=last_etag, modified=last_modified)
+
 last_etag = feed.get("etag")
 last_modified = feed.get("headers").get("Last-Modified")
-feed_update = feedparser.parse(url, etag=last_etag, modified=last_modified)
-if feed_update.status == 304:
-    print("No change")
+if feed.status == 304:
+    print("No change.")
 
-print(feed_update.status)
+for entry in feed.get("entries"):
+    summary = clean_summary(entry.get("summary"))
+    if pttrn.search(summary):
+        # https://stackoverflow.com/questions/1697815/how-do-you-convert-a-python-time-struct-time-object-into-a-datetime-object
+        # https://stackoverflow.com/questions/4770297/convert-utc-datetime-string-to-local-datetime-with-python
+        struct = entry.get("published_parsed")
+        utc = datetime.fromtimestamp(mktime(struct)).replace(tzinfo=from_zone)
+        local = utc.astimezone(to_zone)
+        print(local.strftime("%a %b %d %-I:%M %p"))
+        print(summary)
+  
+    # TODO sometimes strings have html in them, sometimes they have \xa0
+    # https://stackoverflow.com/questions/26068832/how-to-remove-this-xa0-from-a-string-in-python
+
+# TODO locally store last_etag & last_modified
+# with open('results.txt', 'w') as outfile:
+#     json.dump(feed_info, outfile, indent=4, ensure_ascii=False)
